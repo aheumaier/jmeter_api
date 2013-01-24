cf53edfa6b022b226d11294977ae8a2157d28937
aheumaier <a.heumaier@metaways.de>
2013-01-24 11:23:08 +0100

    fixed Rails.root path

--- lib/jmeter-dsl/jmeter_core.rb
+++ lib/jmeter-dsl/jmeter_core.rb
@@ -71,7 +71,7 @@ module JmeterDsl
       end
 
       def change_to_workdir
-        Dir.chdir('/home/aheumaier/codebox/site/public/bin')
+        Dir.chdir(Rails.root+'public/bin')
       end
     end
   end
