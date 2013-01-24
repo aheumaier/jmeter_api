2a1497cea0fc782487ba2881138a67fd37241a45
bauer <bauer.christof@guj.de>
2013-01-24 13:01:46 +0100

    rest_client.rake

--- app/controllers/api/v1/jmeter_runs_controller.rb
+++ app/controllers/api/v1/jmeter_runs_controller.rb
@@ -1,16 +1,25 @@
 ##
-# Module API
-
+# API-Module
+#
+# This encapsulates all the API functionality.
+#
 module Api
 
   ##
   # Versioning
-
+  # 
+  # This enables us to administer multiple versions of the API without changing the core.
+  #
   module V1
 
     ##
-    # This class represents the API part for jmeter_run_controller
-    
+    # This class holds all methods to interact with the JMeter-Client-API.
+    # 
+    # Accepts only JSON input.
+    #
+    # [Args]   small domestic animal
+    # [+Args+] command to copy standard input
+    #
     class JmeterRunsController < ApplicationController
       respond_to :json
 
@@ -23,26 +32,38 @@ module Api
       end
 
       ##
+      #
       # Creates a new JMeter-Run
       #
-      # * *Args*    :
-      #   - :project_id ->
-      #   - :jmeter_run ->
-      # * *Returns* :
-      #   -
-      # * *Raises* :
-      #   - ++ ->
+      # [+Args+]
+      #   - +project_id+ ->
+      #   - +jmeter_run+ -> hash of {"jmx_definition_file_id", "description"}
+      #   
+      # Args::   small domestic animal
+      # +Args+:: command to copy standard input
       #
-
+      # = Level One Heading
+      # == Level Two Heading
+      #
+      # * *Args*:
+      #   - +project_id+ ->
+      #   - +jmeter_run+ -> hash of {"jmx_definition_file_id", "description"}
+      # * *Returns*:
+      #   - +jmeter_run.id+ -> the id of the just created run
+      # * *Raises*:
+      #   - +ArgumentError+ -> if any value is nil or negative
+      #
       def create
-
-        puts "---"*20
-        puts params.inspect
-
         @project ||=  Project.find_by_param(params[:project_id])
+        puts @project.inspect
+
         @jmeter_run = @project.jmeter_runs.build(params[:jmeter_run])
+        puts @jmeter_run.inspect
+
         if @jmeter_run.save
           render :json => @jmeter_run.id
+        else
+          render :json => "JMeter-Run NOT created!"
         end
       end
 
@@ -58,6 +79,17 @@ module Api
         respond_with JmeterRun.status(params[:id])
       end
 
+      ##
+      # Starts a JMeter-Run
+      #
+      # * *Args*:
+      #   - +id+ -> The ID of the JMeter-Run in question
+      # * *Returns*:
+      #   - +String+ -> Just a status message.
+      # * *Raises*:
+      #   - +ArgumentError+ -> if any value is nil or negative
+      #
+      #
       def start
         @jmeter_run ||=  JmeterRun.find(params[:id])
         @project = @jmeter_run.project_id
